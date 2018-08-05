edea8598d259b9c1760c04ce45b334f285ebc8a0
Debodirno Chandra <debodirnochandra@gmail.com>
2018-08-05 15:42:04 +0530

    Added grep solution

--- Scripting/Basic_Shell_Commands/Primers/Grep.sh
+++ Scripting/Basic_Shell_Commands/Primers/Grep.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+#The following command prints all the lines of the file input which contains a number
+#
+#cat input | grep '[0-9]*'
+#
+#Example:
+#Assume that the input has the following content:
+#
+#axes12
+#w143th
+#qwer
+#Then the given command prints the following ouput:
+#
+#axes12
+#w143th
+#
+#
+#Now change the command slighly so that it prints only the number part of the lines.
+#
+
 cat input | grep -o '[0-9]*'
